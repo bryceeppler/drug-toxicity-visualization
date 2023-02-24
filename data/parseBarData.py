7735d3dc6358acc33ef2e03d333023ca46d4e93a
Bryce Eppler <eppler97@gmail.com>
2023-02-24 05:34:09 -0800

    matching csv format in json parsing

--- data/parseBarData.py
+++ data/parseBarData.py
@@ -1,7 +1,6 @@
 import csv
 import json
 #  Right now this only fetches the opioids
-# Open the CSV file
 with open('SubstanceHarmsData.csv', 'r') as csv_file:
     reader = csv.DictReader(csv_file)
 
@@ -15,16 +14,10 @@ with open('SubstanceHarmsData.csv', 'r') as csv_file:
             continue
         if row['Substance'] != 'Opioids':
             continue
-        # make sure row[J] is Male or Female
 
-        # Check if the row is for "by quarter" data
-        # if row['Time_Period'] == 'By year':
         if True:
             # Extract the year and quarter from the Time_Period column
             year = row['Year_Quarter']
-            # year = yearQuarter[:4].strip()[2:] # 2018 -> 18
-            # quarter = yearQuarter[4:].strip() # Q1, Q2, Q3, Q4
-            # month = map Q1 = 1, Q2 = 4, Q3 = 7, Q4 = 10
             month = 1
             value = row['Value']
 
@@ -32,41 +25,106 @@ with open('SubstanceHarmsData.csv', 'r') as csv_file:
             sex = row['Disaggregator']
             age = row['Aggregator']
             date = f'{year}-{month:02}'
-            # Append a new row to the output data list
-            # output_data[date][provice][sex][age] = value
-            # "2016-01": {
-            #  "Alberta": {
-            #   "male": {
-            #     "0-19":20,
-            #     "20-49":30,
-            #     "50+":10
-            #   },
-            #   "female": {
-            #     "0-19":20,
-            #     "20-49":30,
-            #     "50+":10
-            #   }
-            #  },
-            #  "British Columbia": {
-            #       ...
-            # },
-            # Append a new row to the output data list
             if date not in output_data:
                 output_data[date] = {}
             if province not in output_data[date]:
-                output_data[date][province] = {'date': date}
-            if sex not in output_data[date][province]:
-                output_data[date][province][sex] = {}
-            output_data[date][province][sex][age] = value
+                output_data[date][province] = [{'group': sex.lower()}]
 
-    # for every object in output_data:
-    #     remove the date key from each province
-    for date in output_data:
-        for province in output_data[date]:
-            del output_data[date][province]['date']
-            
+            found = False
+            # if an object exists with this group, add the age to it
+            for x in output_data[date][province]:
+                try:
+                    if x["group"] == sex.lower():
+                        x[age] = value
+                        found = True
+                        break
+                except:
+                    pass
+            if not found:
+                objectToAdd = {
+                    "group":sex.lower(),
+                    age:value
+                }
+                output_data[date][province].append(objectToAdd)
 
+               #     "Canada": [
+    #         {
+    #             "group": "female",
+    #             "0 to 19 years": "5",
+    #             "20 to 29 years": "5",
+    #             "30 to 39 years": "4",
+    #             "40 to 49 years": "5",
+    #             "50 to 59 years": "6",
+    #             "60 years or more": "2"
+    #         }, 
+            # objToAdd = {
+            #     "group":sex.lower(),
+            #     age:value
+            # }
+            # output_data[date][province]
+            # if sex not in output_data[date][province]:
+            #     output_data[date][province][sex] = {}
+            # output_data[date][province][sex][age] = value
 
+    # for date in output_data:
+    #     for province in output_data[date]:
+    #         del output_data[date][province]['date']
+
+    # print(output_data)
+    # we have
+    # {
+    # "2021-01": {
+    #     "Canada": {
+    #       "Female": {
+    #         "0 to 19 years": "35",
+    #         "20 to 29 years": "347",
+    #         "30 to 39 years": "496",
+    #         "40 to 49 years": "329",
+    #         "50 to 59 years": "275",
+    #         "60 years or more": "91"
+    #       },
+    #       "Male": {
+    #         "0 to 19 years": "63",
+    #         "20 to 29 years": "735",
+    #         "30 to 39 years": "1276",
+    #         "40 to 49 years": "1046",
+    #         "50 to 59 years": "930",
+    #         "60 years or more": "382"
+    #       }
+    #     },
+    #     "British Columbia": {
+    #     ...
+    #     }
+    #   }
+    # }
+
+    # we need
+    # {
+    # "2021-01": {
+    #     "Canada": [
+    #         {
+    #             "group": "female",
+    #             "0 to 19 years": "5",
+    #             "20 to 29 years": "5",
+    #             "30 to 39 years": "4",
+    #             "40 to 49 years": "5",
+    #             "50 to 59 years": "6",
+    #             "60 years or more": "2"
+    #         },
+    #         {
+    #             "group": "male",
+    #             "0 to 19 years": "20",
+    #             "20 to 29 years": "30",
+    #             "30 to 39 years": "20",
+    #             "40 to 49 years": "20",
+    #             "50 to 59 years": "18",
+    #             "60 years or more": "20"
+    #         }
+    #     ],
+    #     "British Columbia": [
+    #         {...},
+    #      ]
+    #   }
 
 
     # Write the output data to a new json file
